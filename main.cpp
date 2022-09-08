a920311fded9ce0f1f0ab881faf4601283a7ed7d
PeterJanosi <92621566+PeterJanosi@users.noreply.github.com>
2022-09-08 19:18:09 +0200

    Update main.cpp
    hibák javítása

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100; 
+const int N_ELEMENTS = 100; 
 
 int main()
 {
-    int *b = new int[NELEMENTS];// hiábs konstans név
-    std::cout << '1-100 ertekek duplazasa' // nincs ;, ' ' helyett " ", végén legyen új sor "\n".
-    for (int i = 0;) // for ciklus paraméterei hibásak
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa\n"; 
+    for (int i = 0;i < N_ELEMENTS;i++) 
     {
-        b[i] = i * 2; // 0-99-ig számol és nem 1-100-ig, 
+        b[i] = (i+1) * 2; , 
     }
-    for (int i = 0; i; i++) // nem i-ig kell számolni hanem N_ELEMENTS-ig
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        std::cout << "Ertek:"//"\n" hozzáadása a jobb átláthatóság miatt, nem kapunk értéket.
+        std::cout << "Ertek:"<<b[i]<<"\n";
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // int helyett double, kezdőérék megadása
-    for (int i = 0; i < N_ELEMENTS, i++)// rossz elválasztás
+    double atlag=0; 
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]// hiányzó ;
+        atlag += b[i];// hiányzó ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
